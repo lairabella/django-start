1579c4b3c1c3d09516a297e9e744dfe4d2e5fd38
Luis Airabella <luisairabella@gmail.com>
2022-01-21 05:23:02 -0300

    xs

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(your_domain.com www.your_domain.com)
+domains=(dev.biometricpro.io www.dev.biometricpro.io)
 rsa_key_size=4096
 data_path="./data/certbot"
 email="" # Adding a valid address is strongly recommended
